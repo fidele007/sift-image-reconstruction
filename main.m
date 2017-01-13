d9ce61c5783c3bbc07c7ac80b924ea3f7b6b2278
fidele007 <forcefidele@gmail.com>
2017-01-13 02:01:58 +0100

    Fix line endings for main.m

--- main.m
+++ main.m
@@ -1,1 +1,4 @@
-% [sifts, meta] = siftgeo_read('../siftgeo/100000.siftgeo');[sifts, meta] = siftgeo_read('../cat.siftgeo');size(sifts)size(meta)
+[sifts, meta] = siftgeo_read('cat.siftgeo');
+
+size(sifts)
+size(meta)
