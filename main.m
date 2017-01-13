b8ac9c389dbb8400c113d2364b124caee24b8078
fidele007 <forcefidele@gmail.com>
2017-01-13 02:06:15 +0100

    Add TODOs

--- main.m
+++ main.m
@@ -1,4 +1,13 @@
 [sifts, meta] = siftgeo_read('cat.siftgeo');
-
 size(sifts)
 size(meta)
+
+% for each descriptor, search its nearest neighbor in the external database
+
+% recover the corresponding elliptic image patch
+
+% warp the patch
+
+% stich all patches
+
+% complete remaining empty zones
