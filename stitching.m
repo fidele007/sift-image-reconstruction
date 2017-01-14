bfc9d52a5cfae4ef97ca3f3a41727324db3e86f3
herilalaina <rkt.herilalaina@gmail.com>
2017-01-14 23:01:45 +0100

    Fix sum bug

--- stitching.m
+++ stitching.m
@@ -8,7 +8,7 @@ function img = stiching(I, x, y, r, im_patch)
 %  disp(size(I(floor(x-dx/2):floor(x+dx/2 - 1), floor(y-dy/2):floor(y+dy/2 - 1), :)));
   for xx=1:dx
     for yy=1:dy
-      if sum(im_patch(xx, yy) > 0)
+      if sum(im_patch(xx, yy, :)) > 0
         img(floor(x-dx/2 - 1 + xx), floor(y-dy/2 -1 + yy), :) = im_patch(xx, yy, :);
       endif
     endfor
