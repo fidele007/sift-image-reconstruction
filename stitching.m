01d0976d9afa8f7f03bcce526efa29e04961ab3b
herilalaina <rkt.herilalaina@gmail.com>
2017-01-14 22:39:33 +0100

    Remove border

--- stitching.m
+++ stitching.m
@@ -6,7 +6,13 @@ function img = stiching(I, x, y, r, im_patch)
   [dx dy r] = size(im_patch);
 %  disp(size(im_patch));
 %  disp(size(I(floor(x-dx/2):floor(x+dx/2 - 1), floor(y-dy/2):floor(y+dy/2 - 1), :)));
-  img(floor(x-dx/2):floor(x+dx/2 - 1), floor(y-dy/2):floor(y+dy/2 - 1), :) = im_patch;
+  for xx=1:dx
+    for yy=1:dy
+      if sum(im_patch(xx, yy) > 0)
+        img(floor(x-dx/2 - 1 + xx), floor(y-dy/2 -1 + yy), :) = im_patch(xx, yy, :);
+      endif
+    endfor
+  endfor
 endfunction
 
 
@@ -17,5 +23,5 @@ title("Original image");
 
 figure;
 n_img = stiching(img, 500, 100, 20, p_1);
-imshow(n_img);
 title("Patch image");
+imshow(n_img);
