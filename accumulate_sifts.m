d5a579f5cce74aa179ab6a33a1f377e149734b7d
fidele007 <forcefidele@gmail.com>
2017-01-15 03:23:53 +0100

    Add accumulate_sifts.m to store all external SIFT in a big file

--- accumulate_sifts.m
+++ accumulate_sifts.m
@@ -1,10 +1,10 @@
-% Store all SIFTs and its metadata of the external image database
+% Store all SIFT and its metadata of the external image database
 
 siftFiles = glob('../siftgeo/*.siftgeo');
 allSifts = [];
 allMeta = [];
 
-for i=1:numel(siftFiles)
+for i=1:100
   [~, name] = fileparts(siftFiles{i});
   [exSift, exMeta] = siftgeo_read(['../siftgeo/' name '.siftgeo']);
   allSifts = [allSifts;exSift];
